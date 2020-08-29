9a10c5932df816fb05431f3831c9da4c4e9af906
Venkateswara VP <venkateswara.vp@gmail.com>
2020-08-29 09:26:57 -0400

    Added the list feature

--- src/app/astro-list/astro-list.component.ts
+++ src/app/astro-list/astro-list.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { AstroServiceService } from '../astro-service.service';
 import { AstroResponse } from '../astro.response';
+import {NgbCalendar, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-astro-list',
   templateUrl: './astro-list.component.html',
   styleUrls: ['./astro-list.component.scss']
 })
-export class AstroListComponent implements OnInit {
+export class AstroListComponent implements OnInit, OnChanges {
   response: AstroResponse[];
-  constructor(private astroService: AstroServiceService) { }
+  model: NgbDateStruct;
+  date: {year: number, month: number};
+  constructor(private astroService: AstroServiceService, private calendar: NgbCalendar) { }
 
   ngOnInit(): void {
-    this.response = this.astroService.getListWithTransisionsInGMT(8, 2020);
+    this.calendar.getToday();
+    this.viewList();
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.viewList();
+  }
+
+  viewList(): void {
+    this.response = this.astroService.getListWithTransisionsInGMT(this.date.month, this.date.year);
+  }
+
 
 }
